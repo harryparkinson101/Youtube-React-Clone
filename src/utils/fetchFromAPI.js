bbe05e86cba59306ba313eb243738f1b726bd197
Harry Parkinson <71083434+harryparkinson101@users.noreply.github.com>
2023-03-04 04:11:17 +0000

    changed favicon

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -3,20 +3,20 @@ import axios from 'axios';
 const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
-    url: BASE_URL,
-    //params: {part: 'snippet', videoId: 'M7FIvfx5J10'},
-    params: {
-        maxResults: '50'
-    },
-    headers: {
-      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-      'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-    }
+  url: BASE_URL,
+  //params: {part: 'snippet', videoId: 'M7FIvfx5J10'},
+  params: {
+    maxResults: '50'
+  },
+  headers: {
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
 };
 
 export const fetchFromAPI = async (url) => {
-    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
 
-    return data;
+  return data;
 }
 
